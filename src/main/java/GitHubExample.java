ebeeb72bdcb16c1e2a8d105ebb5c4451db85fd3f
André Faria <andrefaria@bluesoft.com.br>
2014-12-18 16:33:50 -0200

    Using new Feign annotations provided by Spring Cloud

--- src/main/java/GitHubExample.java
+++ src/main/java/GitHubExample.java
@@ -1,33 +1,40 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.feign.FeignClientScan;
 import org.springframework.cloud.netflix.feign.FeignConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Named;
 import java.util.List;
 
 @Configuration
 @EnableAutoConfiguration
-public class GitHubExample extends FeignConfiguration {
+@FeignClientScan
+@RestController
+public class GitHubExample extends FeignConfiguration{
 
     static class Contributor {
         String login;
         int contributions;
     }
 
+    @FeignClient(value="https://api.github.com", loadbalance=false)
     interface GitHub {
         @RequestMapping(method = RequestMethod.GET, value = "/repos/{owner}/{repo}/contributors")
-        List<Contributor> contributors(@PathVariable("owner") String owner, @PathVariable("repo") String repo);
+        List<Contributor> contributors(@RequestParam("owner") String owner, @RequestParam("repo") String repo);
     }
 
-    @Bean
-    public GitHub storeClient() {
-        return loadBalance(GitHub.class, "https://api.github.com");
+    @Autowired
+    private GitHub gitHub;
+
+    @RequestMapping("/")
+    public List<Contributor> home() {
+        return gitHub.contributors("netflix", "hystrix");
     }
 
     public static void main(String[] args) {
